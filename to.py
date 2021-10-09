a3daac0e75c25b1ebca52d90d99fcfb9abbaf8d2
Leandro Gonzalez Montesino <44241464+Leandroglez39@users.noreply.github.com>
2021-10-09 13:37:58 -0400

    Create to.py

---
+++ to.py
@@ -0,0 +1,29 @@
+import libtorrent as lt
+import time
+
+ses = lt.session()
+ses.listen_on(6881, 6891)
+
+params = {
+    'save_path': '/content/',
+    'storage_mode': lt.storage_mode_t(2) 
+  }
+
+link = "magnet:?xt=urn:btih:C6B892C9C34393509B162327AAE19C65CDC1E8F5&dn=React+%26amp%3B+Django+Full+Stack%3A+Advanced&tr=udp%3A%2F%2Finferno.demonoid.pw%3A3391%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftorrent.gresille.org%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Fshadowshq.yi.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce"
+handle = lt.add_magnet_uri(ses, link, params)
+ses.start_dht()
+
+print('downloading metadata...')
+
+while (not handle.has_metadata()):
+    time.sleep(1)
+print('got metadata, starting torrent download...')
+
+while (handle.status().state != lt.torrent_status.seeding):
+    s = handle.status()
+    state_str = ['queued', 'checking', 'downloading metadata', \
+            'downloading', 'finished', 'seeding', 'allocating']
+    print('%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+            (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+            s.num_peers, state_str[s.state]))
+    time.sleep(5)
